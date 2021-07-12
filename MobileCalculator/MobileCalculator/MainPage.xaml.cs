5c0c0e46614698ae3c82c40e0ea4a66ec5e554ce
Ryan Alcorn <ryanalcorn88@gmail.com>
2021-07-12 10:22:18 -0700

    Fixed solve button issue and other smaller bugs.

--- MobileCalculator/MobileCalculator/MainPage.xaml.cs
+++ MobileCalculator/MobileCalculator/MainPage.xaml.cs
@@ -13,13 +13,15 @@ namespace MobileCalculator
     {
         List<string> currentSequence = new List<string>();
         HashSet<char> numbers = new HashSet<char> { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-        //HashSet<char> operations = new HashSet<char> { '+', '-', '×', '÷' };
+        HashSet<string> operations = new HashSet<string> { "+", "-", "×", "÷" };
+
+        bool operationFlag = false;
 
         public MainPage()
         {
             InitializeComponent();
 
-            List<Button> buttons = new List<Button>() { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, Clear, Addition, Subtraction, Division, Multiplication };
+            List<Button> buttons = new List<Button>() { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, Clear, Equals, Addition, Subtraction, Division, Multiplication };
 
             foreach(Button b in buttons)
             {
@@ -31,20 +33,42 @@ namespace MobileCalculator
         {
             if(numbers.Contains(character[0]))
             {
+                Debug.WriteLine("Number clicked");
                 Display.Text += character[0];
             }
             else if(character.Equals("Clear"))
             {
+                Debug.WriteLine("Clear clicked");
                 Display.Text = "";
+
+                if(currentSequence.Count > 0) currentSequence.Clear();
             }
-            else if(character.Equals('='))
+            else if(character.Equals("Solve"))
             {
                 Debug.WriteLine("Equals clicked");
                 Display.Text = runCalculation(Display.Text);
+                operationFlag = false;
             }
-            else if(!Display.Text.Equals(""))
+            else if(!Display.Text.Equals("") && operations.Contains(character) && operationFlag == false)
             {
+                Debug.WriteLine("First operation clicked");
                 Display.Text += " " + character + " ";
+                operationFlag = true;
+            }
+            else if (!Display.Text.Equals("") && operations.Contains(character) && operationFlag)
+            {
+                Debug.WriteLine("Multiple operations clicked");
+
+                string newDisplay = runCalculation(Display.Text);
+
+                if(!newDisplay.Equals(character))
+                {
+                    Display.Text = runCalculation(Display.Text) + " " + character + " ";
+
+                    currentSequence.Clear();
+                    currentSequence.Add(Display.Text);
+                    currentSequence.Add(character);
+                }
             }
         }
 
@@ -52,9 +76,11 @@ namespace MobileCalculator
         {
             string[] sequence = currentSequence.Split(' ');
 
+            if (sequence.Length != 3 || sequence[2].Equals("")) return currentSequence;
+
             int outcome = 0;
 
-            if(sequence[1].Equals('+'))
+            if(sequence[1].Equals("+"))
             {
                 outcome = Int32.Parse(sequence[0]) + Int32.Parse(sequence[2]);
             }
