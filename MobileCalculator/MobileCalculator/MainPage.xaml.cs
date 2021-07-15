5229ce22f20eedef6bbee7412d170226a62f0236
Ryan Alcorn <ryanalcorn88@gmail.com>
2021-07-15 14:33:39 -0700

    Added the Problem class to help simplify existing structure. Also added comments.

--- MobileCalculator/MobileCalculator/MainPage.xaml.cs
+++ MobileCalculator/MobileCalculator/MainPage.xaml.cs
@@ -1,108 +1,104 @@
-﻿using System;
+﻿/* Project: Mobile Calculator
+   Author: Ryan Alcorn
+   Date Updated: 7-15-2021 */
+
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MobileCalculator
 {
     public partial class MainPage : ContentPage
     {
-        HashSet<string> numbers = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-        HashSet<string> operations = new HashSet<string> { "+", "-", "×", "÷" };
-
-        bool operationFlag = false;
+        readonly HashSet<string> numbers = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+        readonly HashSet<string> operations = new HashSet<string> { "+", "-", "×", "÷" };
+        Problem currentProblem = new Problem();
 
         public MainPage()
         {
             InitializeComponent();
 
-            List<Button> buttons = new List<Button>() { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, Clear, Equals, Addition, Subtraction, Division, Multiplication };
+            List<Button> buttons = new List<Button>() { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Zero, Clear, Solve, Addition, Subtraction, Division, Multiplication };
 
             foreach(Button b in buttons)
             {
-                b.Clicked += (sender, e) => { displayClicks(b.Text); };
+                b.Clicked += (sender, e) => { DisplayClicks(b.Text); };
             }
         }
 
-        private void displayClicks(string character)
+        /// <summary>
+        /// This function is called when a button is clicked. It calls different functions based on which button is clicked.
+        /// </summary>
+        private void DisplayClicks(string character)
         {
             if(numbers.Contains(character))
             {
-                Debug.WriteLine("Number clicked");
-                Display.Text += character[0];
+                currentProblem.ChangeNumber(character);
+                Display.Text = currentProblem.ChangeDisplayText();
             }
-            else if(character.Equals("Clear"))
+            else if(operations.Contains(character))
+            {
+                if(currentProblem.SecondNumberFlag)
+                {
+                    SolveProblem();
+                }
+
+                if(currentProblem.FirstNumberFlag)
+                {
+                    currentProblem.ChangeOperation(character);
+                    Display.Text = currentProblem.ChangeDisplayText();
+                }
+            }
+            else if (character.Equals("Clear"))
             {
                 Debug.WriteLine("Clear clicked");
                 Display.Text = "";
-                operationFlag = false;
+                currentProblem.ResetProblem();
             }
-            else if(character.Equals("Solve"))
+            else if (character.Equals("Solve"))
             {
                 Debug.WriteLine("Equals clicked");
-
-                if (canSolveProblem(Display.Text))
-                {
-                    Display.Text = runCalculation(Display.Text);
-                    operationFlag = false;
-                }
-            }
-            else if(!Display.Text.Equals("") && operations.Contains(character) && operationFlag == false)
-            {
-                Debug.WriteLine("First operation clicked");
-                Display.Text += " " + character + " ";
-                operationFlag = true;
-            }
-            else if (!Display.Text.Equals("") && operations.Contains(character) && operationFlag)
-            {
-                Debug.WriteLine("Multiple operations clicked");
-
-                if(canSolveProblem(Display.Text))
-                {
-                    Display.Text = runCalculation(Display.Text) + " " + character + " ";
-                }
+                SolveProblem();
             }
         }
 
-        private bool canSolveProblem(string displayText)
+        /// <summary>
+        /// This function checks if the correct properties have been set. If so, the RunCalculation function is called to display the result of the problem.
+        /// The problems properties are then reset and the display is updated with the result.
+        /// </summary>
+        public void SolveProblem()
         {
-            string newDisplay = runCalculation(displayText);
-
-            if (!newDisplay.Equals(Display.Text))
+            if (currentProblem.OperationFlag && currentProblem.SecondNumberFlag)
             {
-                return true;
+                Display.Text = RunCalculation();
+                currentProblem.ResetProblem();
+                currentProblem.ChangeNumber(Display.Text);
             }
-
-            return false;
         }
 
-        private string runCalculation(string displayText)
+        /// <summary>
+        /// This function performs calculations based on what the operation symbol was set as.
+        /// </summary>
+        /// <returns>returns a string representation of the solution of the problem</returns>
+        private string RunCalculation()
         {
-            string[] sequence = displayText.Split(' ');
-
-            if (sequence.Length != 3 || operations.Contains(sequence[2]) || sequence[2].Equals("")) return displayText;
-
             int outcome = 0;
 
-            if(sequence[1].Equals("+"))
+            if(currentProblem.Operation.Equals("+"))
             {
-                outcome = Int32.Parse(sequence[0]) + Int32.Parse(sequence[2]);
+                outcome = currentProblem.FirstNumber + currentProblem.SecondNumber;
             }
-            else if(sequence[1].Equals("-"))
+            else if(currentProblem.Operation.Equals("-"))
             {
-                outcome = Int32.Parse(sequence[0]) - Int32.Parse(sequence[2]);
+                outcome = currentProblem.FirstNumber - currentProblem.SecondNumber;
             }
-            else if (sequence[1].Equals("×"))
+            else if (currentProblem.Operation.Equals("×"))
             {
-                outcome = Int32.Parse(sequence[0]) * Int32.Parse(sequence[2]);
+                outcome = currentProblem.FirstNumber * currentProblem.SecondNumber;
             }
-            else if (sequence[1].Equals("÷") && !sequence[2].Equals("0"))
+            else if (currentProblem.Operation.Equals("÷") && currentProblem.SecondNumber != 0)
             {
-                outcome = Int32.Parse(sequence[0]) / Int32.Parse(sequence[2]);
+                outcome = currentProblem.FirstNumber / currentProblem.SecondNumber;
             }
 
             return outcome.ToString();
---
+++ MobileCalculator/MobileCalculator/Problem.cs
@@ -0,0 +1,87 @@
+﻿/* Project: Mobile Calculator
+   Author: Ryan Alcorn
+   Date Updated: 7-15-2021 */
+
+namespace MobileCalculator
+{
+    /// <summary>
+    /// This class contains elements to create a problem that can be solved by the calculator.
+    /// These elements include two numbers and an operation to be performed.
+    /// </summary>
+    public class Problem
+    {
+        public int FirstNumber { get; private set; }
+        public int SecondNumber { get; private set; }
+        public string Operation { get; private set; }
+        public bool OperationFlag { get; private set; }
+        public bool FirstNumberFlag { get; private set; }
+        public bool SecondNumberFlag { get; private set; }
+
+        /// <summary>
+        /// This constructor calls the ResetProblem function to set the properties.
+        /// </summary>
+        public Problem()
+        {
+            ResetProblem();
+        }
+
+        /// <summary>
+        /// This function sets the operation to the passed in symbol and changes the flag associated with the property.
+        /// </summary>
+        /// <remarks>
+        /// <param name="symbol">symbol: represents one of the four operations that can be performed on the numbers in the problem.</param>
+        /// </remarks>
+        public void ChangeOperation(string symbol)
+        {
+            OperationFlag = true;
+            Operation = symbol;
+        }
+
+        /// <summary>
+        /// This function changes one of the two numbers based on the flags that have been set so far.
+        /// </summary>
+        /// <remarks>
+        /// <param name="newNumber">newNumber: represents a number that helps build one of the two numbers used in the problem.</param>
+        /// </remarks>
+        public void ChangeNumber(string newNumber)
+        {
+            if (!OperationFlag)
+            {
+                FirstNumber = FirstNumber == 0 ? int.Parse(newNumber) : int.Parse(FirstNumber.ToString() + newNumber);
+                FirstNumberFlag = true;
+            }
+            else
+            {
+                SecondNumber = SecondNumber == 0 ? int.Parse(newNumber) : int.Parse(SecondNumber.ToString() + newNumber);
+                SecondNumberFlag = true;
+            }
+        }
+
+        /// <summary>
+        /// This function changes the text on the label named Display based on the flags that have been set so far.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the text to display.
+        /// </returns>
+        public string ChangeDisplayText()
+        {
+            if (!OperationFlag) return FirstNumber.ToString();
+            if (OperationFlag && !SecondNumberFlag) return FirstNumber.ToString() + " " + Operation;
+
+            return FirstNumber.ToString() + " " + Operation + " " + SecondNumber.ToString();
+        }
+
+        /// <summary>
+        /// This function resets all the properties used by the Problem instance.
+        /// </summary>
+        public void ResetProblem()
+        {
+            FirstNumber = 0;
+            SecondNumber = 0;
+            Operation = "";
+            OperationFlag = false;
+            FirstNumberFlag = false;
+            SecondNumberFlag = false;
+        }
+    }
+}
